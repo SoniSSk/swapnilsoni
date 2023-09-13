dcbb8e49da3a21ea7c36a1270a70561b09920790
SoniSSk <kuldeepsoni3099@gmail.com>
2023-09-14 01:57:24 +0530

    portfolio

---
+++ README.md
@@ -0,0 +1,37 @@
+### Description
+
+A simple portfolio template for developer/designers built with React. 
+
+<!-- ### [live preview](https://sonissk.github.io/swapees/) -->
+
+
+### Features
+
+- Fully Responsive
+- Multi-Page Layout
+- Contact Form With EmailJs
+- React-Bootstrap
+- Edit Content From One Place
+
+### Setup
+
+Get the code
+
+<pre>git clone https://sonissk.github.io/swapees/</pre>
+ 
+Install required dependencies
+
+<pre>yarn install</pre>
+
+
+Start the server
+
+<pre>yarn start</pre>
+
+### More
+
+Modify pages content in  `src/content_option.js`.
+
+### Thanks
+
+If you like this portfolio template don't forget give it a ⭐ 
---
+++ src/content_option.js
@@ -0,0 +1,204 @@
+const logotext = "K Soni";
+const meta = {
+    title: "Kuldeep Soni",
+    description: "I’m Kuldeep Soni Software Engineer,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Kuldeep Soni",
+    animated: {
+        first: 'React Developer',
+        second: "Front-End Developer",
+        third: "Software Engineer",
+        forth: "MERM Stack Developer",
+    },
+    description: "Experienced web and mobile app developer with 2.8+ years' expertise in React.js, JavaScript, HTML, CSS, Redux, and Material UI. Skilled in project management, deadline adherence, and delivering top-notch interfaces.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "Abit about my self",
+    aboutme: "Knowledgeable developer with expertise in building web and mobile applications from scratch. With over 2.8+ years of experience, I possess advanced skills in React.js, JavaScript, and related technologies. I excel in analyzing and managing projects, consistently meeting deadlines and delivering high-quality work. Proficient in HTML, CSS, Redux, and Material UI, I create visually appealing and user-friendly interfaces. Known for my ability to work well under pressure, I strive for excellence in every software development endeavor."
+};
+const worktimeline = [{
+    jobtitle: "Software Engineer",
+    where: "Divami Designs Labs Pvt. Ltd.",
+    date: "Sep-2022 to Present",
+    companyUrl: "https://divami.com/",
+},
+{
+    jobtitle: "Full Stack Engineer",
+    where: "Nftically",
+    date: "Feb-2022 to Sep-2022",
+    companyUrl: "https://nftically.com/",
+},
+{
+    jobtitle: "Software Engineer",
+    where: "Ezylegal",
+    date: "Jan-2021 to Feb-2022",
+    companyUrl: "https://ezylegal.in/",
+},
+];
+
+const skills = [{
+    name: "React.js",
+    value: 100,
+},
+{
+    name: "Next.js",
+    value: 100,
+},
+{
+    name: "Javascript",
+    value: 100,
+},
+{
+    name: "Typescript",
+    value: 100,
+},
+{
+    name: "HTML / CSS",
+    value: 100,
+},
+{
+    name: "Redux",
+    value: 100,
+},
+{
+    name: "Material UI / Bootstrap",
+    value: 100,
+},
+{
+    name: "Git",
+    value: 100,
+},
+{
+    name: "Bitbucket / Jira",
+    value: 100,
+},
+{
+    name: "Figma",
+    value: 100,
+},
+
+
+];
+
+const services = [{
+    title: "AI Educator",
+    link: "https://aieducator.com/",
+    tech: "React.js, Redux, Material UI, Typescript, HTML, CSS.",
+    description: "AI Educator: Revolutionizing e-learning for personalized, inclusive education. Seamlessly integrating into school and beyond, our platform caters to each student's unique needs for a convenient, effective, and enjoyable learning experience."
+},
+{
+    title: "Right Data (RDT.ai)",
+    link: "https://www.getrightdata.com/",
+    tech: "React.js, Redux, Material UI, JavaScript, HTML, CSS, Unit Testing, Jest, Git, Jira.",
+    description: "RightData is a comprehensive Data Product Platform featuring no-code tools to connect, cleanse, prepare, and make data accurate and actionable. It begins with DataFactory, offering easy data engineering with drag-and-drop, supporting various data sources.",
+},
+{
+    title: "NFTically Marketplace",
+    link: "https://nftically.com/",
+    tech: "React.js, Redux, JavaScript, HTML, CSS, Php, Git, Jira, Bootstrap.",
+    description: "NFTICALLY: B2B SaaS for customizable NFT stores. Empowers celebs, influencers, gamers, clubs, and enterprises. User-friendly controls for total platform management, no tech expertise needed.",
+},
+{
+    title: "Ezylegal",
+    link: "https://ezylegal.in/",
+    tech: "Next.js, Redux, JavaScript, HTML, CSS, Php, Git, Jira, Chakra UI.",
+    description: "ezyLegal: AI-driven legal tech platform enhancing accessibility and transparency. Connects clients with lawyers, offering clear services and standard pricing. Aiming to serve underserved Indians, prioritizing customer satisfaction to lead in legal services.",
+},
+];
+
+const dataportfolio = [{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/800/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/550/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "kuldeepapply@gmail.com",
+    YOUR_FONE: "+91-9716626438, +91-8960044346",
+    description: "Experienced web and mobile app developer with 2.8+ years' expertise in React.js, JavaScript, HTML, CSS, Redux, and Material UI. Skilled in project management, deadline adherence, and delivering top-notch interfaces.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_9woe8d3",
+    YOUR_TEMPLATE_ID: "template_m69l807",
+    YOUR_USER_ID: "CcmlPNOVFo7QN3r6h",
+};
+
+const socialprofils = {
+    github: "https://github.com/SoniSSk",
+    facebook: "https://www.facebook.com/Amanrajsas/",
+    linkedin: "https://www.linkedin.com/in/kuldeep-soni-480413155/",
+    // twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
